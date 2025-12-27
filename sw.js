e998d17536b6b224eda7a5b1a98508b8cabb6233
chuyizhy <2380064192@qq.com>
2025-12-27 18:53:42 +0800

    Site updated: 2025-12-27 18:53:42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766827155092";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766832818377";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
